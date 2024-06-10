a464f5a9ad2a4d6d84cfec74e1ab3bea5e93d4d3
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2024-06-10 13:42:05 +0200

    Class Poster - findById Method

--- src/Entity/Poster.php
+++ src/Entity/Poster.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
 class Poster
 {
     private int $id;
@@ -17,5 +20,25 @@ class Poster
     public function getJpeg(): string
     {
         return $this->jpeg;
+    }
+
+    public static function findById(int $id): Poster
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, jpeg
+            FROM poster
+            WHERE id = ?
+            SQL
+        );
+        $stmt ->execute([$id]);
+
+        $poster = $stmt->fetchObject(self::class);
+
+        if ($poster === false) {
+            throw new EntityNotFoundException("Il n'existe aucune affiche avec l'identifiant $id.");
+        }
+
+        return $poster;
     }
 }
