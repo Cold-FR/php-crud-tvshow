9cd75f0130f2b58a449bc834ffb49f4b2b2afd77
davi0063 <clement.david1@etudiant.univ-reims.fr>
2024-06-10 10:22:46 +0200

    Class TVShowCollection - findAll Method

--- src/Entity/Collection/TVShowCollection.php
+++ src/Entity/Collection/TVShowCollection.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+use Entity\TVShow;
+use PDO;
+
 class TVShowCollection
 {
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM tvshow
+            ORDER BY name
+            SQL
+        );
+        $stmt->execute();
 
+        return $stmt->fetchAll(PDO::FETCH_CLASS, TVShow::class);
+    }
 }
