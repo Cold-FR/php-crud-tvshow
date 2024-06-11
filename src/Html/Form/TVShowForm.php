d24ac041919e7e2179fa272fba81edfb251fa82a
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2024-06-11 16:45:09 +0200

    Class TVShowForm - getHtmlForm Method

--- src/Html/Form/TVShowForm.php
+++ src/Html/Form/TVShowForm.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Html\Form;
 
 use Entity\TVShow;
+use Html\StringEscaper;
 
 class TVShowForm
 {
+    use StringEscaper;
 
     private ?TVShow $tvShow;
 
@@ -19,5 +21,45 @@ class TVShowForm
     public function getTVShow(): ?TVShow
     {
         return $this->tvShow;
+    }
+
+    public function getHtmlForm(string $action): string
+    {
+        $showName = $this?->getTVShow()?->getName();
+        $showOriginalName = $this?->getTVShow()?->getOriginalName();
+        $showHomepage = $this?->getTVShow()?->getHomepage();
+        $showOverview = $this?->getTVShow()?->getOverview();
+        return <<<HTML
+            <form action="$action" method="post">
+                <input type="hidden" name="id" 
+                        value="{$this?->getTVShow()?->getId()}"/>
+                <label>
+                    Nom
+                    <input type="text" name="name" 
+                        value="{$this->escapeString($showName)}" required/>
+                </label>
+                <label>
+                    Nom original
+                    <input type="text" name="originalName" 
+                        value="{$this->escapeString($showOriginalName)}" required/>
+                </label>
+                <label>
+                    Homepage
+                    <input type="text" name="homepage" 
+                        value="{$this->escapeString($showHomepage)}" required/>
+                </label>
+                <label>
+                    Description
+                    <input type="text" name="overview" 
+                        value="{$this->escapeString($showOverview)}" required/>
+                </label>
+                <label>
+                    Identifiant de l'affiche
+                    <input type="number" name="posterId" 
+                        value="{$this?->getTVShow()?->getPosterId()}"/>
+                </label>
+                <input type="submit" value="Enregistrer"/>
+            </form>
+        HTML;
     }
 }
