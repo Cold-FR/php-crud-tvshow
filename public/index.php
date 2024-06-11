f4ab776f4c6337325af32f37d3a021a48bdd3df9
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2024-06-11 15:10:44 +0200

    index.php & season.php - Clickable list-element

--- public/index.php
+++ public/index.php
@@ -43,18 +43,18 @@ try {
     foreach ($tvShows as $tvShow) {
         $appWebPage->appendContent(
             <<<HTML
-            <li class="list-element">
-                <img class="img-poster" src='poster.php?posterId={$tvShow->getPosterId()}' alt="Affiche de {$tvShow->getName()}">
-                <div class="list-element-info">
-                    <h3 class="list-element-title">
-                        <a href="tvshow.php?tvShowId={$tvShow->getId()}">
-                            {$appWebPage->escapeString($tvShow->getName())}
-                        </a>
-                    </h3>
-                    <p class="list-element-overview">
-                        {$appWebPage->escapeString($tvShow->getOverview())}
-                    </p>
-                </div>
+            <li>
+                <a class="list-element" href="tvshow.php?tvShowId={$tvShow->getId()}">
+                    <img class="img-poster" src='poster.php?posterId={$tvShow->getPosterId()}' alt="Affiche de {$tvShow->getName()}">
+                    <div class="list-element-info">
+                        <h2 class="list-element-title">
+                                {$appWebPage->escapeString($tvShow->getName())}
+                        </h2>
+                        <p class="list-element-overview">
+                            {$appWebPage->escapeString($tvShow->getOverview())}
+                        </p>
+                    </div>
+                </a>
             </li>
             HTML
         );
