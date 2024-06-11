8e61f5d5ca0045f356689337de136c6bf3d32bf8
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2024-06-11 14:07:02 +0200

    Class GenreCollection - findAll Method

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+use Entity\Genre;
+use PDO;
+
 class GenreCollection
 {
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM genre
+            SQL
+        );
 
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Genre::class);
+    }
 }
